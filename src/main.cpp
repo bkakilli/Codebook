ee9123834fdd18648aa81882236c9ab18f4cdd36
Burak Kakillioglu <bkakilli@syr.edu>
2016-04-04 18:50:52 -0400

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <string>
 #include <vector>
 #include <chrono>
 #include <thread>
@@ -16,7 +17,25 @@ double	Codebook::eps = 5;
 int main(int argc, char* argv[])
 {
 	
-	VideoCapture cap("sample.avi"); // open the video file for reading
+	if(argc < 2){
+		cout << "Invalid input." << endl;
+		return -1;
+	}
+	
+	string fileName = argv[1];
+	string type = argv[2];
+	
+	bool train;
+	if(type == "t"){
+		train = true;
+	} else if (type == "p"){
+		train = false;
+	} else {
+		cout << "Unknown parameter; use either t or p." << endl;
+		return -1;
+	}
+	
+	VideoCapture cap(fileName); // open the video file for reading
 
 	if ( !cap.isOpened() ) { // if not success, exit program
 		cout << "Cannot open the video file" << endl;
@@ -24,24 +43,13 @@ int main(int argc, char* argv[])
 	}
 
 	//cap.set(CV_CAP_PROP_POS_MSEC, 300); //start the video at 300ms
-
-	double fps = cap.get(CV_CAP_PROP_FPS); //get the frames per seconds of the video
-	double period_us = 1000000/fps;
-	cout << "Frame per seconds: " << fps << endl;
-	cout << "Period in usecs: " << period_us << endl;
-
-	//namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
-
 	Mat frame;
-	chrono::steady_clock::time_point next = chrono::steady_clock::now();
-	int count = 0;
 	
-	int width = (int) cap.get(CV_CAP_PROP_FRAME_WIDTH);
-	int height = (int) cap.get(CV_CAP_PROP_FRAME_HEIGHT);
-	int frameCount = (int) cap.get(CV_CAP_PROP_FRAME_COUNT);
-	
-	bool train = true;
 	if (train) {
+
+		int width = (int) cap.get(CV_CAP_PROP_FRAME_WIDTH);
+		int height = (int) cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+		int frameCount = (int) cap.get(CV_CAP_PROP_FRAME_COUNT);
 		
 		// Create codebooks
 		Codebook *codebooks = new Codebook[ width*height ];
@@ -63,6 +71,15 @@ int main(int argc, char* argv[])
 	}
 	else {
 		
+		double fps = cap.get(CV_CAP_PROP_FPS); //get the frames per seconds of the video
+		double period_us = 1000000/fps;
+		cout << "Frame per seconds: " << fps << endl;
+		cout << "Period in usecs: " << period_us << endl;
+
+		chrono::steady_clock::time_point next = chrono::steady_clock::now();
+		int count = 0;
+		
+		namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
 		// Read until stream ends
 		while(cap.read(frame)) {
 			
